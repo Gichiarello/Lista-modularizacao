b82408f9d8a463a35a530159ae2849450b792976
GiChiarello <113187714+Gichiarello@users.noreply.github.com>
2023-04-06 18:56:44 -0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,24 +1,29 @@
-/*Saida: quantidade de asteriscos definidos pelo usuario;
-entrada: um numero inteiro;
-criar uma função para calcular a quantidade de asteriscos
-que o programa deve imprimir.
+/*saida: confirmação se a letra esta entre o determinado local;
+entrada: uma letra;
+criar uma função que calcula se tal letra esta ou não dentro de um determinado local.
 */
 #include <stdio.h>
+#include <locale.h>
 
-void asterisco(int n) {
-	int i;
-	for(i=0; i<n; i++){
-		printf("*");
+int ascii(char c){
+	if (c>='a'&& c<='z'){
+		return 1;
 	}
-	
+	else{
+		return 0;
+	}
 }
 
 int main() {
-    int n;
-    printf("Digite um numero: ");
-    scanf("%d", &n);
+    char c;
+    printf("Digite um caractere: ");
+    scanf("%c", &c);
     
-    asterisco(n);
+    if (ascii(c)) {
+        printf("O caractere '%c' esta entre 'a' e 'z'.\n", c);
+    } else {
+        printf("O caractere '%c' nao esta entre 'a' e 'z'.\n", c);
+    }
     
     return 0;
 }
