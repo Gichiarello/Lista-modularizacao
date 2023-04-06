31773843f2bb84672bb48bea422b2dcec94a6061
GiChiarello <113187714+Gichiarello@users.noreply.github.com>
2023-04-05 22:36:57 -0300

    Add files via upload

---
+++ ex2-divisao entre 2 num.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+float div(int x, int y){
+	while(x==0){
+		printf("\nNao e possivel realizar a conta com o divisor sendo 0, insira outro numero:");
+		scanf("%d", &x);
+	}
+	return x*1.0/y;
+}
+void main(){
+	int x, y; 
+	float res;
+	printf("Insira 2 numeros para realizar a divisao:\n");
+	scanf("%d", &x);
+	scanf("%d", &y);
+	res = div(x, y);
+	printf("\ndivisao = %0.2f", res);
+	return 0;
+}
---
+++ ex3.area de um circulo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+float area(float raio, float pi){
+	float x;
+	x = pi*raio*raio;
+	return x;
+}
+void main(){
+	float res, raio, pi=3.14;
+	printf("Insira o valor do raio de um circulo:\n");
+	scanf("%f", &raio);
+	res = area(raio, pi);
+	printf("\nArea do circulo = %.2f", res);
+	return 0;
+}
+
