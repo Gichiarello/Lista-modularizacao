44476190ab1a081148f7a121bc74f4df0ec8c994
GiChiarello <113187714+Gichiarello@users.noreply.github.com>
2023-04-06 18:12:29 -0300

    Add files via upload

---
+++ ex5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <locale.h>
+
+int ascii(char c){
+	if (c>='a'&& c<='z'){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+int main() {
+    char c;
+    printf("Digite um caractere: ");
+    scanf("%c", &c);
+    
+    if (ascii(c)) {
+        printf("O caractere '%c' esta entre 'a' e 'z'.\n", c);
+    } else {
+        printf("O caractere '%c' nao esta entre 'a' e 'z'.\n", c);
+    }
+    
+    return 0;
+}
