30575cfa5b2d3b4dde26ff1bd6a2031cf8c050aa
GiChiarello <113187714+Gichiarello@users.noreply.github.com>
2023-04-06 18:36:52 -0300

    Add files via upload

---
+++ ex6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void asterisco(int n) {
+	int i;
+	for(i=0; i<n; i++){
+		printf("*");
+	}
+	
+}
+
+int main() {
+    int n;
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+    
+    asterisco(n);
+    
+    return 0;
+}
+
